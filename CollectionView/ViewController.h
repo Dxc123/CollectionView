96692b2fb99c1ae265cf3e9d88191426f1216ca7
代星创 <daixingchuang@daixingchuangdemacbook-pro.local>
2017-02-21 21:25:52 +0800

    Initial Commit

---
+++ CollectionView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CollectionView
+//
+//  Created by 代星创 on 2017/2/21.
+//  Copyright © 2017年 Dxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ CollectionView/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CollectionView
+//
+//  Created by 代星创 on 2017/2/21.
+//  Copyright © 2017年 Dxc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
